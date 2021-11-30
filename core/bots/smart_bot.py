5cb728a2050d3a4960d239585f6558272eb8267f
Karelito00 <karelkdv2000@gmail.com>
2021-11-30 17:18:51 -0500

    Add EMA indicator

--- core/bots/smart_bot.py
+++ core/bots/smart_bot.py
@@ -30,6 +30,17 @@ class SmartBot(Bot):
 
         return price_sum / days
 
+    # Exponential Moving Average
+    # EMA = close_price * multiplier + EMA(previous_day) * (1 - multiplier)
+    # multiplier = smoothing / (1 + period)
+    # commonly smoothing is equal to 2
+    def get_EMA(self, count = 10, position = 0, period = 10):
+        multiplier = 2 / (1 + period)
+        ema_today = self.assetAB.asset_data['Close'] * multiplier
+        if(position == 0 or count == 0):
+            return ema_today
+        return ema_today + self.get_EMA(count - 1, position - 1, period) * (1 - multiplier)
+
     def start_bot(self, position = 0):
         while position < self.count_rows:
            day_prices = self.assetAB.get_day_prices(position)
