f78e82f5b03337bd59c12d53878eea321cc5a5b0
Karelito00 <karelkdv2000@gmail.com>
2021-11-30 17:47:52 -0500

    Add macd and signal line

--- core/bots/smart_bot.py
+++ core/bots/smart_bot.py
@@ -30,16 +30,27 @@ class SmartBot(Bot):
 
         return price_sum / days
 
+    def get_close_price(self, position = 0):
+        return self.assetAB.asset_data['Close'][position]
+
     # Exponential Moving Average
     # EMA = close_price * multiplier + EMA(previous_day) * (1 - multiplier)
     # multiplier = smoothing / (1 + period)
     # commonly smoothing is equal to 2
-    def get_EMA(self, count = 10, position = 0, period = 10):
-        multiplier = 2 / (1 + period)
-        ema_today = self.assetAB.asset_data['Close'] * multiplier
+    def get_EMA(self, price_func, count = 10, position = 0, period = 10, smoothing = 2):
+        multiplier = smoothing / (1 + period)
+        ema_today = price_func(position) * multiplier
         if(position == 0 or count == 0):
             return ema_today
         return ema_today + self.get_EMA(count - 1, position - 1, period) * (1 - multiplier)
+
+    # Moving Average Convergence / Divergence
+    def get_MACD(self, position = 0):
+        return self.get_EMA(self.get_close_price ,12, position, 12) - self.get_EMA(self.get_close_price, 26, position, 26)
+
+    # Signal Line is an EMA with macd and period equal to 9
+    def get_signal_line(self, position = 0):
+        return self.get_EMA(self.get_MACD, 9, position, 9)
 
     def start_bot(self, position = 0):
         while position < self.count_rows:
