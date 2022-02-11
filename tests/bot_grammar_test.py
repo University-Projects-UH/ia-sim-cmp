df07ee244e764ffe0b65dbefd26c5a8bda780783
FrankBlanco1 <frank.blanco@estudiantes.matcom.uh.cu>
2022-02-10 19:35:34 -0500

    Add a method to evaluate the attributes of the grammar and perform the bots grammar test

--- tests/bot_grammar_test.py
+++ tests/bot_grammar_test.py
@@ -1,6 +1,9 @@
 from core import BotGrammar
 from core.compiler.parser.slr1 import SLR1Parser
 from core.compiler.parser.parser_lr import LR1Parser
+from core.compiler.lexer.token import Token
+from core.compiler.parser.parser_shift_reduce import evaluate_reverse_parse
+from core.compiler.ast.ast import ProgramNode
 
 def test_answer():
 
@@ -11,4 +14,11 @@ def test_answer():
     parser_lr1 = LR1Parser(G)
     parser_slr1 = SLR1Parser(G)
 
-    assert True == True
+    tokens = [Token('int', G.symbols['int']), Token('x', G.symbols['id']), Token('=', G.symbols['=']), Token('4', G.symbols['int_number']), Token(';', G.symbols[';']), Token('$', G.eof)]
+    token_types = [t.reg_type for t in tokens]
+    right_parse, operations = parser_lr1(token_types, True)
+
+    ast = evaluate_reverse_parse(right_parse, operations, tokens)
+    print(ast)
+
+    assert isinstance(ast, ProgramNode)
--- tests/lr_test.py
+++ tests/lr_test.py
@@ -6,17 +6,19 @@ from core.compiler.bots_grammar import BotGrammar
 from pandas import DataFrame
 from core.compiler.parser.parser_shift_reduce import ShiftReduceParser
 
-G = Grammar()
-E = G.add_non_terminal('E', True)
-A = G.add_non_terminal('A')
-equal, plus, num = G.add_terminals('= + int')
+def test_answer():
 
-E %=  A + equal + A | num
-A %= num + plus + A | num
+    G = Grammar()
+    E = G.add_non_terminal('E', True)
+    A = G.add_non_terminal('A')
+    equal, plus, num = G.add_terminals('= + int')
+
+    E %=  A + equal + A | num
+    A %= num + plus + A | num
 
 
-parser = LR1Parser(G)
+    parser = LR1Parser(G)
 
-derivation = parser([num, plus, num, equal, num, plus, num, G.eof])
+    derivation = parser([num, plus, num, equal, num, plus, num, G.eof])
 
-assert str(derivation) == '[A -> int, A -> int + A, A -> int, A -> int + A, E -> A = A]'
+    assert str(derivation) == '[A -> int, A -> int + A, A -> int, A -> int + A, E -> A = A]'
