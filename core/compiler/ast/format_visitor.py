de4734398aa7aaf9f5c754e348f12918e04822a5
FrankBlanco1 <frank.blanco@estudiantes.matcom.uh.cu>
2022-02-11 01:52:58 -0500

    Fix some details in format_visitor and extend the test for the bots grammar

--- core/compiler/ast/format_visitor.py
+++ core/compiler/ast/format_visitor.py
@@ -1,4 +1,4 @@
-import .visitor as visitor
+from . import visitor as visitor
 from .ast import *
 
 class FormatVisitor(object):
@@ -20,61 +20,74 @@ class FormatVisitor(object):
     @visitor.when(GridBotDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__GridBotDeclarationNode: {node.id}'
-        return f'{ans}'
+        params = '\n'.join(self.visit(param, tabs + 1) for param in node.params)
+        return f'{ans}\n{params}'
 
     @visitor.when(RebalanceBotDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__RebalanceBotDeclarationNode: {node.id}'
-        return f'{ans}'
+        params = '\n'.join(self.visit(param, tabs + 1) for param in node.params)
+        return f'{ans}\n{params}'
 
     @visitor.when(SmartBotDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__SmarteBotDeclarationNode: {node.id}'
+        params = '\n'.join(self.visit(param, tabs + 1) for param in node.params)
+        return f'{ans}\n{params}'
+
+    @visitor.when(AssetDeclarationNode)
+    def visit(self, node, tabs=0):
+        ans = '\t' * tabs + f'\\__AssetDeclarationNode: {node.id} = {node.asset}'
         return f'{ans}'
 
-    @visitor.when(AssetBotDeclarationNode)
+    @visitor.when(AssetsDeclarationNode)
     def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__AssetDeclarationNode: {node.id}'
-        return f'{ans}'
-
-    @visitor.when(AssetsBotDeclarationNode)
-    def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__assetsdeclarationnode: {node.id}'
+        ans = '\t' * tabs + f'\\__AssetsDeclarationNode: {node.id}'
+        assets = '\n'.join(self.visit(asset, tabs + 1) for asset in node.assets)
         return f'{ans}'
 
     @visitor.when(IntDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__ IntDeclarationNode: {node.id}'
-        return ans
+        exp = self.visit(node.expression, tabs + 1)
+        return f'{ans}\n{exp}'
 
     @visitor.when(FloatDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__ FloatDeclarationNode: {node.id}'
-        return ans
+        exp = self.visit(node.expression, tabs + 1)
+        return f'{ans}\n{exp}'
 
     @visitor.when(BoolDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__ BoolDeclarationNode: {node.id}'
-        return ans
+        boolean = self.visit(node.boolean, tabs + 1)
+        return f'{ans}\n{boolean}'
 
     @visitor.when(PortfolioDeclarationNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__ PortfolioDeclarationNode: {node.id}'
-        return ans
+        params = '\n'.join(self.visit(param, tabs + 1) for param in node.params)
+        return f'{ans}\n{params}'
 
     @visitor.when(ReAssignNode)
     def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__ ReAssignNode: id-{node.id} value-{node.value}'
-        return ans
+        #ans = '\t' * tabs + f'\\__ ReAssignNode: id-{node.id} value-{node.value}'
+        ans = '\t' * tabs + f'\\__ ReAssignNode: id-{node.id}'
+        value = self.visit(node.value, tabs + 1)
+        return f'{ans}\n{value}'
 
     @visitor.when(NegateBooleanNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__ NegateBooleanNode'
-        return ans
+        exp = self.visit(node.expression, tabs + 1)
+        return f'{ans}\n{exp}'
 
     @visitor.when(ParenthesisNode)
     def visit(self, node, tabs=0):
-        return '\t' * tabs + f'\\__ ParenthesisNode'
+        ans = '\t' * tabs + f'\\__ ParenthesisNode'
+        exp = self.visit(node.expression, tabs + 1)
+        return f'{ans}\n{exp}'
 
     @visitor.when(EqualNode)
     def visit(self, node, tabs=0):
@@ -83,9 +96,9 @@ class FormatVisitor(object):
         right = self.visit(node.right, tabs + 1)
         return f'{ans}\n{left}\n{right}'
 
-    @visitor.when(NoEqualNode)
+    @visitor.when(NotEqualNode)
     def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__NoEqualNode'
+        ans = '\t' * tabs + f'\\__NotEqualNode'
         left = self.visit(node.left, tabs + 1)
         right = self.visit(node.right, tabs + 1)
         return f'{ans}\n{left}\n{right}'
@@ -120,13 +133,16 @@ class FormatVisitor(object):
 
     @visitor.when(PrintNode)
     def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__PrintNode ${node.elem}'
-        return ans
+        #ans = '\t' * tabs + f'\\__PrintNode ${node.elem}'
+        ans = '\t' * tabs + f'\\__PrintNode'
+        elem = self.visit(node.elem, tabs + 1)
+        return f'{ans}\n{elem}'
 
     @visitor.when(FuncCallNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__FuncCallNode'
-        return ans
+        params = '\n'.join(self.visit(param, tabs + 1) for param in node.params)
+        return f'{ans}\n{params}'
 
     @visitor.when(PlusNode)
     def visit(self, node, tabs=0):
--- tests/bot_grammar_test.py
+++ tests/bot_grammar_test.py
@@ -4,6 +4,7 @@ from core.compiler.parser.parser_lr import LR1Parser
 from core.compiler.lexer.token import Token
 from core.compiler.parser.parser_shift_reduce import evaluate_reverse_parse
 from core.compiler.ast.ast import ProgramNode
+from core.compiler.ast.format_visitor import FormatVisitor
 
 def test_answer():
 
@@ -14,11 +15,34 @@ def test_answer():
     parser_lr1 = LR1Parser(G)
     parser_slr1 = SLR1Parser(G)
 
-    tokens = [Token('int', G.symbols['int']), Token('x', G.symbols['id']), Token('=', G.symbols['=']), Token('4', G.symbols['int_number']), Token(';', G.symbols[';']), Token('$', G.eof)]
+    tokens = [Token('int', G.symbols['int']), 
+    Token('x', G.symbols['id']), 
+    Token('=', G.symbols['=']), 
+    Token('4', G.symbols['int_number']), 
+    Token(';', G.symbols[';']), 
+    Token('x', G.symbols['id']),
+    Token('=', G.symbols['=']),
+    Token('30', G.symbols['int_number']),
+    Token(';', G.symbols[';']),
+    Token('smart_bot', G.symbols['smart_bot']),
+    Token('y', G.symbols['id']),
+    Token('=', G.symbols['=']),
+    Token('smart_bot', G.symbols['smart_bot']),
+    Token('(', G.symbols['(']),
+    Token('12', G.symbols['int_number']),
+    Token(',', G.symbols[',']),
+    Token('x', G.symbols['id']),
+    Token(')', G.symbols[')']),
+    Token(';', G.symbols[';']),
+    Token('$', G.eof)]
+
+
     token_types = [t.reg_type for t in tokens]
     right_parse, operations = parser_lr1(token_types, True)
 
     ast = evaluate_reverse_parse(right_parse, operations, tokens)
-    print(ast)
+    formatter = FormatVisitor()
+    print(formatter.visit(ast))
 
     assert isinstance(ast, ProgramNode)
+    assert True == False
